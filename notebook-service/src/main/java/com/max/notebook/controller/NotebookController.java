0c62cceea4641c02b01007b20ee4d120a7405122
limaoxuan <563157030@qq.com>
2020-12-18 16:22:43 +0800

    TEST-2 sonarcube

--- notebook-service/src/main/java/com/max/notebook/controller/NotebookController.java
+++ notebook-service/src/main/java/com/max/notebook/controller/NotebookController.java
@@ -40,7 +40,7 @@ public class NotebookController {
 		if (result.hasErrors()) {
 			return "add-notebook";
 		}
-
+//<!--test sornarqube-->
 		notebookRepository.save(notebook);
 		return "redirect:list";
 	}

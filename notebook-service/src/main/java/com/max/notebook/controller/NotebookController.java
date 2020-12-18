164ae916b761fa9ae87bea4f731f3a2c12a656e9
limaoxuan <563157030@qq.com>
2020-12-18 16:55:50 +0800

    TEST-2 ss

--- notebook-service/src/main/java/com/max/notebook/controller/NotebookController.java
+++ notebook-service/src/main/java/com/max/notebook/controller/NotebookController.java
@@ -40,7 +40,7 @@ public class NotebookController {
 		if (result.hasErrors()) {
 			return "add-notebook";
 		}
-//<!--test sornarqube-->
+//<!--test sornarq-->
 		notebookRepository.save(notebook);
 		return "redirect:list";
 	}
